09d370af3313c9dbb46bec81a9d669715eb456d8
Raffaele Intorcia <raffaele.intorcia@gmail.com>
2024-05-16 22:08:45 +0200

    Vdc wip

--- lib/vdc.asm
+++ lib/vdc.asm
@@ -32,6 +32,7 @@
 #importonce
 
 #import "labels/lib/kernal.asm"
+#import "labels/lib/screeneditor.asm"
 #import "labels/lib/vdc.asm"
 
 .filenamespace c128lib
@@ -397,6 +398,272 @@ InitText: {
     ora $beef;
     ldx #$1A;
     stx $d600; bit $d600; bpl *-3; sta $d601
+}
+
+/**
+ * @brief Read from Vdc internal memory and write it to Vic screen memory by
+ * using coordinates.
+ *
+ * @param[in] xPos X coord on Vdc screen
+ * @param[in] yPos Y coord on Vdc screen
+ * @param[in] destination Vic screen memory absolute address
+ * @param[in] qty Number of byte to copy
+ *
+ * @note Use c128lib_ReadFromVdcMemoryByCoordinates in vdc-global.asm
+ *
+ * @since 1.1.0
+ */
+.macro ReadFromVdcMemoryByCoordinates(xPos, yPos, destination, qty) {
+  .errorif (xPos == -1 && yPos != -1), "xPos and yPos must be -1 at same time"
+  .errorif (xPos != -1 && yPos == -1), "xPos and yPos must be -1 at same time"
+  .errorif (xPos < -1 || yPos < -1), "xPos and yPos can't be lower than -1"
+  .errorif (qty <= 0), "qty must be greater than 0"
+  .errorif (qty > 255), "qty must be lower than 256"
+  .if (xPos != -1 && yPos != -1) {
+    ldx #$12
+    lda #>getTextOffset80Col(xPos, yPos)
+    jsr ScreenEditor.WRITEREG
+    lda #<getTextOffset80Col(xPos, yPos)
+    inx
+    jsr ScreenEditor.WRITEREG
+  }
+    ldy #0
+  CopyLoop:
+    jsr ScreenEditor.READ80
+    sta destination, y
+    iny
+    cpy #qty
+    bne CopyLoop
+}
+.asserterror "ReadFromVdcMemoryByCoordinates(-1, 0, $beef, 100)", { ReadFromVdcMemoryByCoordinates(-1, 0, $beef, 100) }
+.asserterror "ReadFromVdcMemoryByCoordinates(0, -1, $beef, 100)", { ReadFromVdcMemoryByCoordinates(0, -1, $beef, 100) }
+.asserterror "ReadFromVdcMemoryByCoordinates(-2, 0, $beef, 100)", { ReadFromVdcMemoryByCoordinates(-2, 0, $beef, 100) }
+.asserterror "ReadFromVdcMemoryByCoordinates(0, -2, $beef, 100)", { ReadFromVdcMemoryByCoordinates(0, -2, $beef, 100) }
+.asserterror "ReadFromVdcMemoryByCoordinates(-2, -2, $beef, 100)", { ReadFromVdcMemoryByCoordinates(-2, -2, $beef, 100) }
+.asserterror "ReadFromVdcMemoryByCoordinates(2, 2, $beef, 0)", { ReadFromVdcMemoryByCoordinates(2, 2, $beef, 0) }
+.asserterror "ReadFromVdcMemoryByCoordinates(2, 2, $beef, 256)", { ReadFromVdcMemoryByCoordinates(2, 2, $beef, 256) }
+.assert "ReadFromVdcMemoryByCoordinates(-1, -1, $beef, 100)", { ReadFromVdcMemoryByCoordinates(-1, -1, $beef, 100) },
+{
+    ldy #0; jsr $CDD8; sta $beef, y; iny; cpy #100; bne *-9;
+}
+.assert "ReadFromVdcMemoryByCoordinates(1, 1, $beef, 100)", { ReadFromVdcMemoryByCoordinates(1, 1, $beef, 100) },
+{
+    ldx #$12; lda #0; jsr $CDCC; lda #81; inx; jsr $CDCC;
+    ldy #0; jsr $CDD8; sta $beef, y; iny; cpy #100; bne *-9;
+}
+.assert "ReadFromVdcMemoryByCoordinates(1, 1, $beef, 255)", { ReadFromVdcMemoryByCoordinates(1, 1, $beef, 255) },
+{
+    ldx #$12; lda #0; jsr $CDCC; lda #81; inx; jsr $CDCC;
+    ldy #0; jsr $CDD8; sta $beef, y; iny; cpy #255; bne *-9;
+}
+
+/**
+ * @brief Read from Vdc internal memory and write it to Vic screen memory by
+ * using source address.
+ *
+ * @param[in] source Vdc memory absolute address
+ * @param[in] destination Vic screen memory absolute address
+ * @param[in] qty Number of byte to copy
+ *
+ * @note Use c128lib_ReadFromVdcMemoryByAddress in vdc-global.asm
+ *
+ * @since 1.1.0
+ */
+.macro ReadFromVdcMemoryByAddress(source, destination, qty) {
+  .errorif (qty <= 0), "qty must be greater than 0"
+  .errorif (qty > 255), "qty must be lower than 256"
+    ldx #$12
+    lda #>source
+    jsr c128lib.ScreenEditor.WRITEREG
+    lda #<source
+    inx
+    jsr c128lib.ScreenEditor.WRITEREG
+
+    ldy #0
+  CopyLoop:
+    jsr c128lib.ScreenEditor.WRITE80
+    sta destination, y
+    iny
+    cpy #qty
+    bne CopyLoop
+}
+.asserterror "ReadFromVdcMemoryByAddress($beef, $baab, 0)", { ReadFromVdcMemoryByAddress($beef, $baab, 0) }
+.asserterror "ReadFromVdcMemoryByAddress($beef, $baab, 256)", { ReadFromVdcMemoryByAddress($beef, $baab, 256) }
+.assert "ReadFromVdcMemoryByAddress($beef, $baab, 100)", { ReadFromVdcMemoryByAddress($beef, $baab, 100) },
+{
+    ldx #$12; lda #$be; jsr $CDCC; lda #$ef; inx; jsr $CDCC;
+    ldy #0; jsr $CDCA; sta $baab, y; iny; cpy #100; bne *-9;
+}
+.assert "ReadFromVdcMemoryByAddress($beef, $baab, 255)", { ReadFromVdcMemoryByAddress($beef, $baab, 255) },
+{
+    ldx #$12; lda #$be; jsr $CDCC; lda #$ef; inx; jsr $CDCC;
+    ldy #0; jsr $CDCA; sta $baab, y; iny; cpy #255; bne *-9;
+}
+
+/**
+ * @brief Read from Vic screen memory and write it to Vdc internal memory by
+ * using coordinates.
+ *
+ * @param[in] xPos X coord on Vic screen
+ * @param[in] yPos Y coord on Vic screen
+ * @param[in] destination Vdc internal memory absolute address
+ * @param[in] qty Number of byte to copy
+ *
+ * @note Use c128lib_WriteToVdcMemoryByCoordinates in vdc-global.asm
+ *
+ * @since 1.1.0
+ */
+.macro WriteToVdcMemoryByCoordinates(source, xPos, yPos, qty) {
+  .errorif (xPos == -1 && yPos != -1), "xPos and yPos must be -1 at same time"
+  .errorif (xPos != -1 && yPos == -1), "xPos and yPos must be -1 at same time"
+  .errorif (xPos < -1 || yPos < -1), "xPos and yPos can't be lower than -1"
+  .errorif (qty <= 0), "qty must be greater than 0"
+  .errorif (qty > 255), "qty must be lower than 256"
+  .if (xPos != -1 && yPos != -1) {
+    ldx #$12
+    lda #>getTextOffset80Col(xPos, yPos)
+    jsr c128lib.ScreenEditor.WRITEREG
+    lda #<getTextOffset80Col(xPos, yPos)
+    inx
+    jsr c128lib.ScreenEditor.WRITEREG
+  }
+    ldy #0
+  CopyLoop:
+    lda source, y
+    jsr c128lib.ScreenEditor.WRITE80
+    iny
+    cpy #qty
+    bne CopyLoop
+}
+.asserterror "WriteToVdcMemoryByCoordinates($beef, -1, 0, 100)", { WriteToVdcMemoryByCoordinates($beef, -1, 0, 100) }
+.asserterror "WriteToVdcMemoryByCoordinates($beef, 0, -1, 100)", { WriteToVdcMemoryByCoordinates($beef, 0, -1, 100) }
+.asserterror "WriteToVdcMemoryByCoordinates($beef, -2, 0, 100)", { WriteToVdcMemoryByCoordinates($beef, -2, 0, 100) }
+.asserterror "WriteToVdcMemoryByCoordinates($beef, 0, -2, 100)", { WriteToVdcMemoryByCoordinates($beef, 0, -2, 100) }
+.asserterror "WriteToVdcMemoryByCoordinates($beef, -2, -2, 100)", { WriteToVdcMemoryByCoordinates($beef, -2, -2, 100) }
+.asserterror "WriteToVdcMemoryByCoordinates($beef, 2, 2, 0)", { WriteToVdcMemoryByCoordinates($beef, 2, 2, 0) }
+.asserterror "WriteToVdcMemoryByCoordinates($beef, 2, 2, 256)", { WriteToVdcMemoryByCoordinates($beef, 2, 2, 256) }
+.assert "WriteToVdcMemoryByCoordinates($beef, -1, -1, 100)", { WriteToVdcMemoryByCoordinates($beef, -1, -1, 100) },
+{
+    ldy #0; lda $beef, y; jsr $CDCA; iny; cpy #100; bne *-9;
+}
+.assert "WriteToVdcMemoryByCoordinates($beef, 1, 1, 100)", { WriteToVdcMemoryByCoordinates($beef, 1, 1, 100) },
+{
+    ldx #$12; lda #0; jsr $CDCC; lda #81; inx; jsr $CDCC;
+    ldy #0; lda $beef, y; jsr $CDCA; iny; cpy #100; bne *-9;
+}
+.assert "WriteToVdcMemoryByCoordinates($beef, 1, 1, 255)", { WriteToVdcMemoryByCoordinates($beef, 1, 1, 255) },
+{
+    ldx #$12; lda #0; jsr $CDCC; lda #81; inx; jsr $CDCC;
+    ldy #0; lda $beef, y; jsr $CDCA; iny; cpy #255; bne *-9;
+}
+
+/**
+ * @brief Read from Vic screen memory and write it to Vdc internal memory by
+ * using coordinates.
+ *
+ * @param[in] source Vdc memory absolute address
+ * @param[in] destination Vic screen memory absolute address
+ * @param[in] qty Number of byte to copy
+ *
+ * @note Use c128lib_WriteToVdcMemoryByAddress in vdc-global.asm
+ *
+ * @since 1.1.0
+ */
+.macro WriteToVdcMemoryByAddress(source, destination, qty) {
+  .errorif (qty <= 0), "qty must be greater than 0"
+  .errorif (qty > 255), "qty must be lower than 256"
+    ldx #$12
+    lda #>destination
+    jsr c128lib.ScreenEditor.WRITEREG
+    lda #<destination
+    inx
+    jsr c128lib.ScreenEditor.WRITEREG
+
+    ldy #0
+  CopyLoop:
+    lda source, y
+    jsr c128lib.ScreenEditor.WRITE80
+    iny
+    cpy #qty
+    bne CopyLoop
+}
+.asserterror "WriteToVdcMemoryByAddress($beef, $baab, 0)", { WriteToVdcMemoryByAddress($beef, $baab, 0) }
+.asserterror "WriteToVdcMemoryByAddress($beef, $baab, 256)", { WriteToVdcMemoryByAddress($beef, $baab, 256) }
+.assert "WriteToVdcMemoryByAddress($beef, $baab, 100)", { WriteToVdcMemoryByAddress($beef, $baab, 100) },
+{
+    ldx #$12; lda #$ba; jsr $CDCC; lda #$ab; inx; jsr $CDCC;
+    ldy #0; lda $beef, y; jsr $CDCA; iny; cpy #100; bne *-9;
+}
+.assert "WriteToVdcMemoryByAddress($beef, $baab, 255)", { WriteToVdcMemoryByAddress($beef, $baab, 255) },
+{
+    ldx #$12; lda #$ba; jsr $CDCC; lda #$ab; inx; jsr $CDCC;
+    ldy #0; lda $beef, y; jsr $CDCA; iny; cpy #255; bne *-9;
+}
+
+/**
+ * @brief Calculates memory offset of text cell specified by given coordinates
+ * on 80 cols screen
+ *
+ * @param[in] xPos X coord on Vdc screen
+ * @param[in] yPos Y coord on Vdc screen
+ * @return Memory offset of Vdc specified coordinate
+ *
+ * @note Use c128lib_WriteToVdcMemoryByAddress in vdc-global.asm
+ *
+ * @since 1.1.0
+ */
+.function getTextOffset80Col(xPos, yPos) {
+  .return xPos + Vdc.TEXT_SCREEN_80_COL_WIDTH * yPos
+}
+.assert "getTextOffset80Col(0,0) gives 0", getTextOffset80Col(0, 0), 0
+.assert "getTextOffset80Col(79,0) gives 79", getTextOffset80Col(79, 0), 79
+.assert "getTextOffset80Col(0,1) gives 80", getTextOffset80Col(0, 1), 80
+.assert "getTextOffset80Col(19,12) gives 979", getTextOffset80Col(19, 12), 979
+.assert "getTextOffset80Col(79,24) gives 1999", getTextOffset80Col(79, 24), 1999
+
+/**
+ * @brief Returns the address start of Vdc display memory data. This
+ * is stored in Vdc register SCREEN_MEMORY_STARTING_HIGH_ADDRESS and
+ * SCREEN_MEMORY_STARTING_LOW_ADDRESS.
+ * The 16-bit value is stored in $FB and $FC.
+ *
+ * @note Use c128lib_GetVdcDisplayStart in vdc-global.asm
+ *
+ * @since 1.1.0
+ */
+.macro GetVdcDisplayStart() {
+  ldx #Vdc.SCREEN_MEMORY_STARTING_HIGH_ADDRESS
+  ReadVdc()
+
+  sta $fb
+  inx
+  ReadVdc()
+  sta $fc
+}
+
+/**
+ * @brief Set the pointer to the RAM area that is to be updated.
+ * The update pointer is stored in Vdc register CURRENT_MEMORY_HIGH_ADDRESS
+ * and CURRENT_MEMORY_LOW_ADDRESS.
+ *
+ * @param[in] address Address of update area
+ *
+ * @note Use c128lib_SetVdcUpdateAddress in vdc-global.asm
+ *
+ * @since 1.1.0
+ */
+.macro SetVdcUpdateAddress(address) {
+  ldx #Vdc.CURRENT_MEMORY_HIGH_ADDRESS
+  lda #>address
+  WriteVdc();
+
+  inx
+  .var a1 = <address
+  .var a2 = >address
+  .if (a1 != a2) {
+    lda #<address // include if different from hi-byte.
+  }
+  WriteVdc()
 }
 
 /**
