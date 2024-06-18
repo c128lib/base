ca0ca54bb125c8e19a4f05757649cec42606c812
intoinside <intoinside@users.noreply.github.com>
2024-06-18 18:16:21 +0000

    deploy: fd05947eeef089662bd92c04721d7c6b5e3d47b2

--- mmu-global_8asm.js
+++ mmu-global_8asm.js
@@ -1,5 +1,8 @@
 var mmu_global_8asm =
 [
     [ "c128lib_SetMMUConfiguration", "mmu-global_8asm.html#a70dc6e9f686455e98ff2ad8035b0fe05", null ],
-    [ "c128lib_SetMMULoadConfiguration", "mmu-global_8asm.html#a4e5266f100b800a3513f982dfb34fa04", null ]
+    [ "c128lib_SetMMULoadConfiguration", "mmu-global_8asm.html#a4e5266f100b800a3513f982dfb34fa04", null ],
+    [ "c128lib_SetBankConfiguration", "mmu-global_8asm.html#a4528d96721af23cc376ce6436c83ffb1", null ],
+    [ "c128lib_SetModeConfig", "mmu-global_8asm.html#a20bb607ea450a330ceb33b9ef73775f7", null ],
+    [ "c128lib_SetCommonRAM", "mmu-global_8asm.html#a33e9daff46f2f31773e8701df11939c0", null ]
 ];
--- mmu_8asm.js
+++ mmu_8asm.js
@@ -1,5 +1,8 @@
 var mmu_8asm =
 [
     [ "SetMMUConfiguration", "mmu_8asm.html#a0e367e41fc25de49987703726aed4474", null ],
-    [ "SetMMULoadConfiguration", "mmu_8asm.html#a8028a51bdb8333c33e0a385e82b54716", null ]
+    [ "SetMMULoadConfiguration", "mmu_8asm.html#a8028a51bdb8333c33e0a385e82b54716", null ],
+    [ "SetBankConfiguration", "mmu_8asm.html#afed26fccb43d0f7053fd361a273d0ed4", null ],
+    [ "SetModeConfig", "mmu_8asm.html#a7af66efa2e8684b329aef1a7124c9f12", null ],
+    [ "SetCommonRAM", "mmu_8asm.html#a95264c496f278c41ef154133319b9d95", null ]
 ];
